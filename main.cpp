7a39c2ff099cc87f18d7c43abdd2334cac985ccb
Anton Burmagin <burmagin.anton.s@gmail.com>
2024-04-09 21:55:41 +0300

    first commit: solve 2nd task

--- main.cpp
+++ main.cpp
@@ -1,5 +1,49 @@
 #include <iostream>
 
+void findIndexMaxSum (int arr[]) {
+    int start = 0, end = 0, startTemp = 0, endTemp = 0;
+    int sumMax = 0, sumTemp = 0;
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+        if (arr[i] >= 0) {
+            if(sumMax > 0){
+                if((sumTemp + arr[i]) > 0) {
+                    sumMax += sumTemp;
+                }
+            }
+            sumTemp += arr[i];
+            endTemp = i;
+
+        } else {
+            if (sumTemp > sumMax) {
+                sumMax = sumTemp;
+                start = startTemp;
+                end = endTemp;
+                startTemp = i;
+                endTemp = i;
+            }
+            sumTemp += arr[i];
+            endTemp = i;
+        }
+    }
+}
+
 int main() {
-    
+    // 15.6.1 
+    int arr[] = {-2, 1, -3, 4, -1 ,2 ,1 ,-5 ,4};
+
+
+    // 15.6.2 find two numbers which gives Rusult as sum
+    int a[] = {2, 7, 11, 15, -8, 24, 13, 0, 55};
+    int result = 55;
+    for (int i = 0; i < sizeof(a) / sizeof(a[0]) - 1; i++) {
+        for (int j = i+1; j < sizeof(a) / sizeof(a[0]); j++) {
+            if (a[i] + a[j] == result) {
+                std::cout << a[i] << " + " << a[j] << " = " << result << std::endl;
+                return 0;
+            }
+        }
+    }
+    std::cout << "No two number give " << result << " as sum!" << std::endl;
+
+
 }
